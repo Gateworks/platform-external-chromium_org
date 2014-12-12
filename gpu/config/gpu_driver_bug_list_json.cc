6b5044c3c31708eab28967cc213c002aaca9a4e1
Bart Sears <bsears@google.com>
2014-12-11 17:31:26 -0800

    m40 fork: Update volantis workaround to cover L MR0.5 (5.0.2)
    BUG: 18725105
    Change-Id: I384fde4c9b36c948e995841469b1061da763584d
    (cherry picked from commit 5ea7c0fdf34dbf084fc7a0f6c540de3a2e0a24f3)

--- gpu/config/gpu_driver_bug_list_json.cc
+++ gpu/config/gpu_driver_bug_list_json.cc
@@ -1063,7 +1063,7 @@ LONG_STRING_CONST(
         "type": "android",
         "version": {
           "op": "<=",
-          "value": "5.0.1"
+          "value": "5.0.2"
         }
       },
       "gl_vendor": "NVIDIA.*",
